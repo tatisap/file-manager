2d683b35caad7588a684c4e5f63ed9b8266a34ad
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-06-10 18:37:03 +0300

    feat: import modules to index.js

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ import { argv, stdin, stdout } from 'process';
 import readline from 'readline';
 import os from 'os';
 
+import { create } from './modules/create.js';
+import { remove } from './modules/delete.js';
+import { copy } from './modules/copy.js';
+import { compress } from './modules/compress.js';
+import { decompress } from './modules/decompress.js';
+import { calculateHash } from './modules/hash.js';
+import { getOsPropValue } from './modules/os.js';
+import { read } from './modules/read.js';
+import { rename } from './modules/rename.js';
+
+const commands = {
+  cat: read,
+  add: create,
+  rn: rename,
+  cp: copy,
+  rm: remove,
+  os: getOsPropValue,
+  hash: calculateHash,
+  compress: compress,
+  decompress: decompress,
+};
+
 let currentdir = os.homedir();
 
 const usernameArg = argv.find(arg => arg.startsWith('--username'));
