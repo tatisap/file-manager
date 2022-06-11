6dee6a7789c8c2095bb0e1413a532cab6d8d4aa5
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-06-11 23:12:25 +0300

    fix: implement pipeline into copy function by using promise API

--- modules/copy.js
+++ modules/copy.js
@@ -2,21 +2,20 @@ import path from 'path';
 import { isExist } from './rename.js';
 import { makeAbsolute } from './absPath.js';
 import { createReadStream, createWriteStream } from 'fs';
-import { pipeline } from 'stream';
+import { pipeline } from 'stream/promises';
 
 export const copy = async (srcPath, destDirPath) => {
   const absSrcPath = makeAbsolute(srcPath);
   const absDestPath = path.join(makeAbsolute(destDirPath), path.parse(absSrcPath).base);
   if (await isExist(absDestPath) || !await isExist(absSrcPath)) throw new Error('FS operation failed');
-
-  const source = createReadStream(absSrcPath);
-  const dest = createWriteStream(absDestPath);
-
-  pipeline(
-    source,
-    dest,
-    (err) => {
-      if (err) return console.error(err);
-    }
-  );
+  
+  try {
+    await pipeline(
+      createReadStream(absSrcPath),
+      createWriteStream(absDestPath),
+    );
+  }
+  catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
