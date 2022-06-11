b27f127f02c65a14d3d13d84c16342f8170b376b
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-06-11 20:55:05 +0300

    fix: fix create function

--- modules/create.js
+++ modules/create.js
@@ -1,8 +1,10 @@
+import path from 'path';
 import { writeFile } from 'fs/promises';
+import { cwd } from 'process';
 
-export const create = async (path) => {
+export const create = async (fileName) => {
   try {
-    await writeFile(path, '', {flag:'wx'});
+    await writeFile(path.join(cwd(), fileName), '', {flag:'wx'});
   }
   catch (err) {
     if (err.code === 'EEXIST') throw new Error('FS operation failed');
