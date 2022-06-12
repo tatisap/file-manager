048ffbd7458b4d298be0ace316f34edfac36d469
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-06-12 19:32:01 +0300

    fix: promosify pipeline

--- modules/fs/compress.js
+++ modules/fs/compress.js
@@ -1,23 +1,26 @@
 import { createReadStream, createWriteStream } from 'fs';
 import path from 'path';
-import { pipeline } from 'stream';
+import { pipeline } from 'stream/promises';
 import { createBrotliCompress } from 'zlib';
 import { makeAbsolute } from '../common/make-path-absolute.js';
 import { isExist } from '../common/validation.js';
 
 export const compress = async (srcPath, destDirPath) => {
-  if (!await isExist(makeAbsolute(srcPath))) throw new Error('FS operation failed');
+  const absSrcPath = makeAbsolute(srcPath);
+  if (!await isExist(absSrcPath)) throw new Error('FS operation failed');
 
-  const source = createReadStream(makeAbsolute(srcPath));
-  const dest = createWriteStream(path.join(makeAbsolute(destDirPath), 'archive'));
+  const source = createReadStream(absSrcPath);
+  const dest = createWriteStream(path.join(makeAbsolute(destDirPath), `${path.basename(absSrcPath)}.br`));
   const brotli = createBrotliCompress();
 
-  pipeline(
-    source,
-    brotli,
-    dest,
-    (err) => {
-      if (err) return console.error(err)
-    }
-  );
+  try {
+    await pipeline(
+      source,
+      brotli,
+      dest
+    );
+  }
+  catch (err) {
+    throw new Error('Operation failed');
+  }
 };
--- modules/fs/decompress.js
+++ modules/fs/decompress.js
@@ -1,25 +1,27 @@
 import { createReadStream, createWriteStream } from 'fs';
 import path from 'path';
-import { pipeline } from 'stream';
+import { pipeline } from 'stream/promises';
 import { createBrotliDecompress } from 'zlib';
 import { makeAbsolute } from '../common/make-path-absolute.js';
 import { isExist } from '../common/validation.js';
 
 export const decompress = async (srcPath, destDirPath) => {
   const absSrcPath = makeAbsolute(srcPath);
-  const absDestPath = path.join(makeAbsolute(destDirPath), 'archive');
+  const absDestPath = path.join(makeAbsolute(destDirPath), path.basename(absSrcPath, '.br'));
   if (!await isExist(absSrcPath)) throw new Error('FS operation failed');
 
   const source = createReadStream(absSrcPath);
   const dest = createWriteStream(absDestPath);
   const brotli = createBrotliDecompress();
 
-  pipeline(
-    source,
-    brotli,
-    dest,
-    (err) => {
-      if (err) return console.error(err)
-    }
-  );
+  try {
+    await pipeline(
+      source,
+      brotli,
+      dest
+    );
+  }
+  catch (err) {
+    throw new Error('Operation failed');
+  }
 };
