3656bc377c5555849f31eaee50b504b6d23f4c1b
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-19 16:07:51 +0300

    fix: fix move file bug

--- modules/common/make-path-absolute.js
+++ modules/common/make-path-absolute.js
@@ -2,5 +2,6 @@ import path, { isAbsolute } from 'path';
 import { cwd } from 'process';
 
 export const makeAbsolute = (pathway) => {
-  return (isAbsolute(pathway)) ? pathway : path.join(cwd(), pathway);
+  const absPathway = (isAbsolute(pathway)) ? pathway : path.join(cwd(), pathway);
+  return absPathway;
 }
--- modules/fs/move.js
+++ modules/fs/move.js
@@ -1,10 +1,10 @@
 import { makeAbsolute } from "../common/make-path-absolute.js";
-import { copy } from "./copy.js"
-import { cwd } from 'process';
+import { copy } from "./copy.js";
 import { remove } from "./remove.js";
+import path from 'path';
 
 export const move = async (srcPath, destDirPath) => {
-  if (makeAbsolute(destDirPath) === cwd()) return;
+  if (makeAbsolute(destDirPath) === path.dirname(makeAbsolute(srcPath))) return;
   await copy(srcPath, destDirPath);
   await remove(srcPath);
 }
